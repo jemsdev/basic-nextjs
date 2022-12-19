768f909505fa7200309b8cf5ac46cc251b401808
Jamal <jamal14262@gmail.com>
2022-12-19 15:48:56 +0700

    getServerSideProps

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,9 +1,34 @@
 import Layout from "../../component/layout";
+import style from '../../styles/Blog.module.css';
 
-export default function index() {
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+interface BlogProps {
+  dataBlog: Post[]
+}
+export default function index(props: BlogProps) {
+  const {dataBlog} = props;
   return (
     <Layout pageTitle="Blog Page">
-      <p>This Blog</p>
+      {dataBlog.map((blog) => (        
+          <div key={blog.id} className={style.card}>
+            <h3>{blog.title}</h3>
+            <p>{blog.body}</p>
+          </div>        
+      ))}
     </Layout>
-  )
+  );
+}
+
+export async function getServerSideProps(){
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const dataBlog = await res.json();
+  return {
+    props: {
+      dataBlog
+    },
+  };
 }
